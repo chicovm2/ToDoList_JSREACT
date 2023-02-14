32c55f55aabfe09573a7660d6d2333718b9c339f
Francisco Nunes Pinheiro <124183034+chicovm2@users.noreply.github.com>
2023-02-13 21:27:47 -0300

    Add files via upload

---
+++ src/components/FirstComponent.js
@@ -0,0 +1,15 @@
+function FirstComponent(){
+
+    //Algum Comentário
+
+    return(
+        <div className ='firstcomponent'>
+            {/*Comentário JSX*/}
+            <p>Primeiro Componente</p>
+            <label htmlFor="name">Nome:</label>
+            <input type='text' name="name"/>
+        </div>
+    )
+}
+
+export default FirstComponent
